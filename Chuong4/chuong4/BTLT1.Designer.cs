dc1f5b651824f3087728e18b4f1ca94be5832e7f
thienbao2507 <108049743+thienbao2507@users.noreply.github.com>
2023-12-27 14:46:00 +0700

    Add files via upload

---
+++ Chuong4/chuong4/BTLT1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace chuong4
+{
+    partial class BTLT1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btRtoL = new System.Windows.Forms.Button();
+            this.btLtoR = new System.Windows.Forms.Button();
+            this.btRemove = new System.Windows.Forms.Button();
+            this.btClearAll = new System.Windows.Forms.Button();
+            this.btExit = new System.Windows.Forms.Button();
+            this.btInsert = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập chuỗi";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Enabled = false;
+            this.txtBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox1.Location = new System.Drawing.Point(28, 70);
+            this.txtBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(209, 32);
+            this.txtBox1.TabIndex = 1;
+            this.txtBox1.Text = "nguuu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(406, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Chọn màu";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Cyan",
+            "Red",
+            "Blue",
+            "Yellow"});
+            this.comboBox1.Location = new System.Drawing.Point(355, 69);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(209, 34);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(31, 173);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(207, 259);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 22;
+            this.listBox2.Location = new System.Drawing.Point(356, 173);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(207, 246);
+            this.listBox2.TabIndex = 3;
+            // 
+            // btRtoL
+            // 
+            this.btRtoL.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btRtoL.Location = new System.Drawing.Point(242, 175);
+            this.btRtoL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btRtoL.Name = "btRtoL";
+            this.btRtoL.Size = new System.Drawing.Size(110, 42);
+            this.btRtoL.TabIndex = 4;
+            this.btRtoL.Text = "==>";
+            this.btRtoL.UseVisualStyleBackColor = true;
+            this.btRtoL.Click += new System.EventHandler(this.btRtoL_Click);
+            // 
+            // btLtoR
+            // 
+            this.btLtoR.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLtoR.Location = new System.Drawing.Point(242, 229);
+            this.btLtoR.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btLtoR.Name = "btLtoR";
+            this.btLtoR.Size = new System.Drawing.Size(110, 42);
+            this.btLtoR.TabIndex = 4;
+            this.btLtoR.Text = "<==";
+            this.btLtoR.UseVisualStyleBackColor = true;
+            this.btLtoR.Click += new System.EventHandler(this.btLtoR_Click);
+            // 
+            // btRemove
+            // 
+            this.btRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btRemove.Location = new System.Drawing.Point(242, 286);
+            this.btRemove.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btRemove.Name = "btRemove";
+            this.btRemove.Size = new System.Drawing.Size(110, 42);
+            this.btRemove.TabIndex = 4;
+            this.btRemove.Text = "Remove";
+            this.btRemove.UseVisualStyleBackColor = true;
+            this.btRemove.Click += new System.EventHandler(this.btRemove_Click);
+            // 
+            // btClearAll
+            // 
+            this.btClearAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btClearAll.Location = new System.Drawing.Point(242, 333);
+            this.btClearAll.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btClearAll.Name = "btClearAll";
+            this.btClearAll.Size = new System.Drawing.Size(110, 42);
+            this.btClearAll.TabIndex = 4;
+            this.btClearAll.Text = "Clear All";
+            this.btClearAll.UseVisualStyleBackColor = true;
+            this.btClearAll.Click += new System.EventHandler(this.btClearAll_Click);
+            // 
+            // btExit
+            // 
+            this.btExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btExit.Location = new System.Drawing.Point(242, 388);
+            this.btExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(110, 42);
+            this.btExit.TabIndex = 4;
+            this.btExit.Text = "Exit";
+            this.btExit.UseVisualStyleBackColor = true;
+            this.btExit.Click += new System.EventHandler(this.btExit_Click);
+            // 
+            // btInsert
+            // 
+            this.btInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btInsert.Location = new System.Drawing.Point(29, 106);
+            this.btInsert.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btInsert.Name = "btInsert";
+            this.btInsert.Size = new System.Drawing.Size(98, 31);
+            this.btInsert.TabIndex = 5;
+            this.btInsert.Text = "Insert";
+            this.btInsert.UseVisualStyleBackColor = true;
+            this.btInsert.Click += new System.EventHandler(this.btInsert_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(220, 116);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(98, 57);
+            this.panel1.TabIndex = 6;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(45, 22);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // BTLT1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 473);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btInsert);
+            this.Controls.Add(this.btExit);
+            this.Controls.Add(this.btClearAll);
+            this.Controls.Add(this.btRemove);
+            this.Controls.Add(this.btLtoR);
+            this.Controls.Add(this.btRtoL);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "BTLT1";
+            this.Text = "BTLT1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btRtoL;
+        private System.Windows.Forms.Button btLtoR;
+        private System.Windows.Forms.Button btRemove;
+        private System.Windows.Forms.Button btClearAll;
+        private System.Windows.Forms.Button btExit;
+        private System.Windows.Forms.Button btInsert;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
